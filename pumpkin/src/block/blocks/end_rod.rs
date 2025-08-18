42420107d26ff0b9f1f0e78cf4b985d3d1498d13
ht06 <hthug06@gmail.com>
2025-08-18 16:38:12 +0200

    fix: placement + particles (all Done)

--- pumpkin/src/block/blocks/end_rod.rs
+++ pumpkin/src/block/blocks/end_rod.rs
@@ -1,15 +1,14 @@
 use crate::block::{BlockBehaviour, OnPlaceArgs, RandomTickArgs};
 use async_trait::async_trait;
-use rand::Rng;
-use pumpkin_data::{BlockDirection, FacingExt};
-use pumpkin_data::block_properties::{Axis, EndRodLikeProperties};
 use pumpkin_data::block_properties::BlockProperties;
+use pumpkin_data::block_properties::EndRodLikeProperties;
 use pumpkin_data::particle::Particle;
+use pumpkin_data::{Block, FacingExt};
 use pumpkin_macros::pumpkin_block;
-use pumpkin_protocol::java::client::play::ArgumentType::Vec3;
 use pumpkin_util::math::vector3::Vector3;
 use pumpkin_world::BlockStateId;
-use pumpkin_world::generation::Direction;
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
 
 #[pumpkin_block("minecraft:end_rod")]
 pub struct EndRodBlock;
@@ -18,37 +17,53 @@ pub struct EndRodBlock;
 impl BlockBehaviour for EndRodBlock {
     async fn on_place(&self, args: OnPlaceArgs<'_>) -> BlockStateId {
         let mut props = EndRodLikeProperties::default(args.block);
-        props.facing = args.direction.to_facing();
-        if args.world.get_block(&args.use_item_on.position).await.eq(args.block) {
+
+        let blockstate = args
+            .world
+            .get_block_state_id(&args.position.offset(args.direction.to_offset()))
+            .await;
+
+        if Block::from_state_id(blockstate).eq(args.block)
+            && EndRodLikeProperties::from_state_id(blockstate, args.block).facing
+                == args.direction.to_facing().opposite()
+        {
+            props.facing = args.direction.to_facing();
+        } else {
             props.facing = args.direction.to_facing().opposite();
         }
-        
+
         props.to_state_id(args.block)
     }
     async fn random_tick(&self, args: RandomTickArgs<'_>) {
-        let direction = EndRodLikeProperties::from_state_id(args.world.get_block_state_id(args.position).await, args.block)
-            .facing
-            .to_block_direction();
+        let direction = EndRodLikeProperties::from_state_id(
+            args.world.get_block_state_id(args.position).await,
+            args.block,
+        )
+        .facing
+        .to_block_direction();
 
-        let mut random = rand::rng();
+        let mut random = StdRng::from_os_rng();
 
         let x = args.position.0.x as f64 + 0.55 - (random.random::<f64>() * 0.1);
         let y = args.position.0.y as f64 + 0.55 - (random.random::<f64>() * 0.1);
         let z = args.position.0.z as f64 + 0.55 - (random.random::<f64>() * 0.1);
 
         let offset = 0.4 - (random.random::<f64>() + random.random::<f64>()) * 0.4;
-        
-        if random.random::<i16>() == 5 {
-            args.world.spawn_particle(
-                Vector3::new((x + direction.to_offset().x as f64) * offset,
-                             (y + direction.to_offset().y as f64) * offset,
-                             (z + direction.to_offset().z as f64) * offset),
-                Vector3::new(random.random::<f32>() * 0.005, random.random::<f32>() * 0.005, random.random::<f32>() * 0.005),
-                5.0,
-                3,
-                Particle::EndRod
-            ).await;
-        }
 
+        if random.random::<i16>() == 5 {
+            args.world
+                .spawn_particle(
+                    Vector3::new(
+                        (x + direction.to_offset().x as f64) * offset,
+                        (y + direction.to_offset().y as f64) * offset,
+                        (z + direction.to_offset().z as f64) * offset,
+                    ),
+                    Vector3::new(0.0, 0.0, 0.0),
+                    5.0,
+                    3,
+                    Particle::EndRod,
+                )
+                .await;
+        }
     }
 }
